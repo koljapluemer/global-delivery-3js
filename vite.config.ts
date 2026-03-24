d7732737469682447c26577266f2c2443e2d8514
Kolja Sam <github@koljapluemer.com>
2026-03-24 16:35:23 +0100

    itch settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
+  base: './', // critical for itch deployment
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
